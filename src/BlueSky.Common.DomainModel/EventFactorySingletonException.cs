87b16e3aa5ec25d2d5bddf26950fe4e079ae89c0
yusahaz <yusahaz@gmail.com>
2018-03-15 15:58:57 +0300

    Add Common Projects

---
+++ src/BlueSky.Common.DomainModel/EventFactorySingletonException.cs
@@ -0,0 +1,32 @@
+﻿namespace BlueSky.Common.DomainModel
+{
+    using Core;
+    using System;
+
+    public class EventFactorySingletonException : BlueSkyException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventFactorySingletonException"/> class.
+        /// </summary>
+        public EventFactorySingletonException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventFactorySingletonException"/> class.
+        /// </summary>
+        public EventFactorySingletonException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventFactorySingletonException"/> class.
+        /// </summary>
+        public EventFactorySingletonException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
