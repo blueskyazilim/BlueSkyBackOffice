13e3b151efa1625524f794eb40133a42f6a2b212
yusahaz <yusahaz@gmail.com>
2018-03-21 18:24:35 +0300

    Add Product

---
+++ src/BlueSky.Commerce.DomainModel/Product.cs
@@ -0,0 +1,51 @@
+﻿namespace BlueSky.Commerce.DomainModel
+{
+    using Common.DomainModel;
+    using Core.DesignByContract;
+    using System;
+    using System.Collections.Generic;
+
+    public class Product : CodedRevisionTrackableEntityBase
+    {
+        protected Product() { }
+
+        protected internal Product(
+            string code,
+            string name,
+            string description) :
+            base(code, name, description)
+        {
+
+        }
+        
+        public virtual IEnumerable<ProductCategory> ProductCategories
+        {
+            get { return this.productCategories; }
+        }
+
+        protected internal ProductCategory AddCategory(Category category, int displayOrder)
+        {
+            var productCategory = new ProductCategory(this, category, displayOrder);
+
+            this.productCategories.Add(productCategory);
+
+            return productCategory;
+        }
+
+        protected internal void RemoveCategory(ProductCategory productCategory)
+        {
+            Require.That(productCategory, Is.NotNull);
+            Require.That(productCategory.Product, Is.NotNull);
+            Require.That(productCategory.Category, Is.NotNull);
+
+            if (this.Id != productCategory.Product.Id)
+            {
+                throw new Exception();
+            }
+
+            this.productCategories.Remove(productCategory);
+        }
+
+        private IList<ProductCategory> productCategories;
+    }
+}
---
+++ src/BlueSky.Commerce.DomainModel/ProductCategory.cs
@@ -0,0 +1,48 @@
+﻿namespace BlueSky.Commerce.DomainModel
+{
+    using Common.DomainModel;
+    using Core.DesignByContract;
+    using System;
+
+    public class ProductCategory : VersionedEntityBase<int>
+    {
+        protected ProductCategory() { }
+
+        protected internal ProductCategory(
+            Product product,
+            Category category,
+            int displayOrder)
+        {
+            this.Product = product;
+            this.Category = category;
+            this.DisplayOrder = displayOrder;
+        }
+
+        public virtual Product Product
+        {
+            get { return this.product; }
+            protected set
+            {
+                Require.That(value, Is.NotNull);
+
+                this.product = value;
+            }
+        }
+
+        public virtual Category Category
+        {
+            get { return this.category; }
+            protected set
+            {
+                Require.That(value, Is.NotNull);
+
+                this.category = value;
+            }
+        }
+
+        public virtual int DisplayOrder { get; set; }
+
+        private Product product;
+        private Category category;
+    }
+}
